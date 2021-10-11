99224e88a6788bbb292fce41d5e5910ebc70612c
Fernando <fer@Fernandos-iMac.local>
2021-10-10 22:16:54 -0500

    listas de usuarios front

--- src/main/java/co/edu/unbosque/tiendavirtualcuatro/frontend/dao/UsuarioDAO.java
+++ src/main/java/co/edu/unbosque/tiendavirtualcuatro/frontend/dao/UsuarioDAO.java
@@ -37,5 +37,32 @@ private static final String URL = "http://localhost:5000";
       return null;
     }
 
+    
+    
+    
   }
+  
+  public String listaDeUsuarios() {
+		
+		try {
+
+          URL url = new URL(URL+"/usuarios/listaDeUsuarios");//your url i.e fetch data from .
+          HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+          conn.setRequestMethod("GET");
+          conn.setRequestProperty("Accept", "application/json");
+          if (conn.getResponseCode() != 200) {
+              throw new RuntimeException("Failed : HTTP Error code : "
+                      + conn.getResponseCode());
+          }
+          InputStreamReader in = new InputStreamReader(conn.getInputStream());
+          BufferedReader br = new BufferedReader(in);
+          String json = br.readLine();
+          conn.disconnect();
+          return json;
+      } catch (Exception e) {
+          System.out.println("Exception in NetClientGet:- " + e);
+      }
+		return null;
+	}	
+
 }
---
+++ src/main/webapp/WEB-INF/jsp/usuarios/listaUsuarios.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+  <%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
+    <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+  
+  <t:plantilla>
+          <h2>Portal de Usuarios</h2>
+          <div><p>${mensaje}</p></div>
+
+<p><a href="/usuarios/nuevo">Crear Usuario</p>
+
+
+
+        </t:plantilla>
